8ae901565ea45df3cd8e06d753b28967de65d7b0
Damon Sharp <damon.sharp@gmail.com>
2017-02-06 01:07:46 -0500

    remove keys

--- inc/config.php
+++ inc/config.php
@@ -1,11 +1,11 @@
 <?php
 
-// Normally these should be outside the web root, but hey, this is a POC.
+// Normally these should be outside the web root or in wp_options via a settings page, but hey, this is a POC.
 // Fill in with your own values...
 
 return $config = [
-	'consumer' => 'F3cAfdAn3YFehQ8Z0XAz7hBFY',
-	'consumer_secret' => 'AVadCG8EOw6V5EN8i552ZBfEtW6IS4Aqext9kIjkS07Dlgx0XQ',
-	'access' => '22008136-XqESkDoh66FmYyXsuXnWw7qH2A32fA3zhS5qKCQCv',
-	'access_secret' => 'dwME7Zy82KhaCldGKEEoVg5QhkfeOgBPktplqHKpkCyWd',
+	'consumer' => '',
+	'consumer_secret' => '',
+	'access' => '',
+	'access_secret' => '',
 ];
